2b9046b991c8b1482500c4ef7d4be8b83153649e
SC131 <amos27109@gmail.com>
2023-06-18 11:42:56 -0500

    REGISTRARDOMICILIO FIX

--- src/components/RegistrarDomicilio/RegistrarDomicilio.js
+++ src/components/RegistrarDomicilio/RegistrarDomicilio.js
@@ -1,29 +1,30 @@
 export default {
-    name: "Domicilio",
-    data(){
-        return{
-            frmDomicilio:{
-                IDDomicilio: "",
-	            Direccion_Dom: "",
-	            Interior_Dom: "",
-	            Piso_Dom: "",
-	            Nomb_Malla_Dom: "",
-	            IDCondicion: "",
-	            IDEstrato: "",
-	            IDPredio: "",
-	            IDDistrito: "",
-            },
-            message: "",
-        };
+  name: "Domicilio",
+  data() {
+    return {
+      frmDomicilio: {
+        IDDomicilio: "",
+        Ubicacion_Dom: "",
+        Direccion_Dom: "",
+        Interior_Dom: "",
+        Piso_Dom: "",
+        Nomb_Malla_Dom: "",
+        IDCondicion: "",
+        IDEstrato: "",
+        IDPredio: "",
+        IDDistrito: "",
+      },
+      message: "",
+    };
+  },
+  methods: {
+    createDomicilio() {
+      this.axios
+        .post("http://localhost:3000/domicilio", this.frmDomicilio)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => e);
     },
-    methods:{
-        createDomicilio(){
-            this.axios
-                .post("http://localhost:3000/domicilio",this.frmDomicilio)
-                .then((res) => {
-                    console.log(res);
-                })
-            .catch((e) => e);
-        },
-    },
+  },
 };
