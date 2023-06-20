339ea7e7272c300659dd6de5934e30bb5af1d0b5
SC131 <amos27109@gmail.com>
2023-06-19 23:09:52 -0500

    Fix Registrar Materiales (igual q de gustavo error en el frmMaterial era frmMateriales y uno extra q seria el template estaba por abajo xd)

--- src/components/registrarMateriales/registrarMateriales.vue
+++ src/components/registrarMateriales/registrarMateriales.vue
@@ -1,55 +1,56 @@
-<template>nprm <div>
+<template>
+  <div>
     <v-app-bar color="green accent-4" dense dark>
-        <v-toolbar-title>KALLPA</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-icon>mdi-account-circle</v-icon>
+      <v-toolbar-title>KALLPA</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-account-circle</v-icon>
     </v-app-bar>
-       <form name="registrarMateriales" @submit.prevent="createMateriales">
-       <div class="Materiales">
-       <h2>Registrar Materiales</h2>
-    <v-text-field
-        label="ID Materiales"
-        placeholder="Ingresa la ID del Material"
-        filled
-        v-model="frmMaterial.IDMateriales"></v-text-field> 
-    <v-text-field
-        label="Nombre Malla"
-        placeholder="Ingresa nombre de la malla"
-        filled
-        v-model="frmMaterial.Nombre_Ma"></v-text-field>
-    <v-text-field
-        label="Unidad de medida"
-        placeholder="Ingresa unidad de medida"
-        filled
-        v-model="frmMaterial.UnidadMedida_Ma"></v-text-field>
-    <v-text-field
-        label="Stock de Materiales"
-        placeholder="Ingresa el Stock de los materiales"
-        filled
-        v-model="frmMaterial.Stock_Ma"></v-text-field>
-</div>
-<v-btm type="submit" class="button-1" depressed color="primary">
-    REGISTRAR MATERIALES
-</v-btm>
-</form>
-</div>
+    <form name="registrarMateriales" @submit.prevent="createMateriales">
+      <div class="Materiales">
+        <h2>Registrar Materiales</h2>
+        <v-text-field
+          label="ID Materiales"
+          placeholder="Ingresa la ID del Material"
+          filled
+          v-model="frmMateriales.IDMateriales"></v-text-field>
+        <v-text-field
+          label="Nombre Malla"
+          placeholder="Ingresa nombre de la malla"
+          filled
+          v-model="frmMateriales.Nombre_Ma"></v-text-field>
+        <v-text-field
+          label="Unidad de medida"
+          placeholder="Ingresa unidad de medida"
+          filled
+          v-model="frmMateriales.UnidadMedida_Ma"></v-text-field>
+        <v-text-field
+          label="Stock de Materiales"
+          placeholder="Ingresa el Stock de los materiales"
+          filled
+          v-model="frmMateriales.Stock_Ma"></v-text-field>
+      </div>
+      <v-btm type="submit" class="button-1" depressed color="primary">
+        REGISTRAR MATERIALES
+      </v-btm>
+    </form>
+  </div>
+</template>
 <style>
-.Contrato {
-    margin: 20px;
-    max-width: 30%;
+.Materiales {
+  margin: 20px;
+  max-width: 30%;
 }
 .button-1 {
-    background: #033076;
-    color: #ffffff;
-    border-color: #232323;
-    border-width: 5px;
-    border-style: solid;
-    border-radius: 3px 3px 3px 3px;
-    font-weight: 400;
-    font-size: 24px;
-    text-align: right;
-    margin: 20px;
+  background: #033076;
+  color: #ffffff;
+  border-color: #232323;
+  border-width: 5px;
+  border-style: solid;
+  border-radius: 3px 3px 3px 3px;
+  font-weight: 400;
+  font-size: 24px;
+  text-align: right;
+  margin: 20px;
 }
 </style>
-</template>
 <script src="./registrarMateriales.js"></script>
