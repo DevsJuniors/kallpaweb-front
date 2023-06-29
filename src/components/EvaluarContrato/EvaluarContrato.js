55d8b450da4bdc38ec5c72b94267cf08ee37b56a
SC131 <amos27109@gmail.com>
2023-06-29 00:59:15 -0500

    Evaluar Contrato ACTUALIZAR ESTADO

--- src/components/EvaluarContrato/EvaluarContrato.js
+++ src/components/EvaluarContrato/EvaluarContrato.js
@@ -1,11 +1,16 @@
 export default {
   data() {
     return {
-      searchEstado: "",
+      searchID: "",
+      numSum: "",
+      estado: "",
+      DNI_cli: "",
+      searchE: "",
       search: "",
       searchF: "",
       contratos: [],
       estados: [],
+      estadosA: ["En revisión", "Observado", "Desaprobado", "Aprobado"],
     };
   },
   computed: {
@@ -21,23 +26,29 @@ export default {
         { text: "Tipo Instalación", value: "IDTipoInst" },
         { text: "DNI Cliente", value: "DNI_cli" },
         { text: "DNI Empleado", value: "DNI_Em" },
-        { text: "Modificar Estado", value: "mestado", sortable: false },
       ];
     },
+
     contratosFiltrados() {
       const searchTerm = this.search.toLowerCase().trim();
       const searchFecha = this.searchF.toLowerCase().trim();
+      const searchEstado = this.searchE.toLowerCase().trim();
 
-      if (!searchTerm && !searchFecha && !this.searchEstado) {
+      if (searchEstado === "ningún estado") {
+        return window.location.reload();
+      }
+      if (!searchTerm && !searchFecha && !searchEstado) {
+        return this.contratos;
+      }
+      if (this.isNingunEstadoSelected) {
         return this.contratos;
       }
 
       return this.contratos.filter((contrato) => {
         const idC = contrato.IDContrato.toString().includes(searchTerm);
         const FeC = contrato.Fecha_Con.toString().includes(searchFecha);
-        const estadoC = contrato.estado
-          .toLowerCase()
-          .includes(this.searchEstado);
+        const estadoC = contrato.estado.toLowerCase().includes(searchEstado);
+
         return idC && FeC && estadoC;
       });
     },
@@ -55,6 +66,54 @@ export default {
         .catch((error) =>
           console.error("Error al obtener los datos de los contratos : ", error)
         );
+    },
+
+    llenarCampos() {
+      const contratoEncontrado = this.contratos.find(
+        (contrato) => contrato.IDContrato.toString() === this.searchID.trim()
+      );
+
+      if (contratoEncontrado) {
+        this.numSum = contratoEncontrado.numSum;
+        this.DNI_cli = contratoEncontrado.DNI_cli;
+        console.log(this.numSum, this.DNI_cli);
+      } else {
+        this.numSum = "";
+        this.DNI_cli = "";
+      }
+    },
+    actualizarEstado() {
+      const idcontrato = this.searchID.trim();
+      const contrato = this.contratos.find(
+        (c) => c.IDContrato.toString() === idcontrato
+      );
+
+      if (contrato) {
+        contrato.estado = this.estado;
+        this.axios
+          .patch(`http://localhost:3000/contrato/${idcontrato}`, {
+            estado: this.estado,
+          })
+          .then((response) => {
+            console.log(
+              "Estado del contrato actualizado exitosamente" + response
+            );
+            // Puedes realizar acciones adicionales después de la actualización exitosa
+          })
+          .catch((error) => {
+            console.error("Error al actualizar el estado del contrato:", error);
+            // Manejo de errores
+          })
+          .finally(() => {
+            // Limpia los campos de texto y el estado después de la actualización
+            this.searchID = "";
+            this.numSum = "";
+            this.DNI_cli = "";
+            this.estado = "";
+          });
+      } else {
+        console.log("Contrato no encontrado");
+      }
     },
   },
 };
--- src/components/EvaluarContrato/EvaluarContrato.vue
+++ src/components/EvaluarContrato/EvaluarContrato.vue
@@ -5,23 +5,64 @@
       :items="contratosFiltrados"
       item-key="IDContrato"
       class="elevation-1">
-      <template v-slot:top>
+      <template slot="top">
         <v-text-field
           v-model="search"
           label="Buscar por ID"
-          class="mx-16"></v-text-field>
+          class="mx-16"
+          solo></v-text-field>
         <v-combobox
-          v-model="searchEstado"
-          :items="contratos.map((contratos) => contratos.estado)"
+          v-model="searchE"
+          :items="[
+            'Ningún Estado',
+            ...contratos.map((contrato) => contrato.estado),
+          ]"
           label="Buscar por estado"
           class="mx-16"
           solo></v-combobox>
         <v-text-field
           v-model="searchF"
           label="Buscar por Fecha"
-          class="mx-16"></v-text-field>
+          class="mx-16"
+          solo></v-text-field>
       </template>
     </v-data-table>
+    <v-container>
+      <p>ID Contrato</p>
+      <v-text-field
+        label="ID Contrato"
+        class="mx-16"
+        solo
+        v-model="searchID"
+        @input="llenarCampos"></v-text-field>
+      <p>Numero Suministro</p>
+      <v-text-field
+        label="Numero Suministro"
+        class="mx-16"
+        solo
+        disabled
+        v-model="numSum"></v-text-field>
+      <p>DNI Cliente</p>
+      <v-text-field
+        label="DNI Cliente"
+        class="mx-16"
+        solo
+        disabled
+        v-model="DNI_cli"></v-text-field>
+      <p>Estado:</p>
+      <v-combobox
+        :items="estadosA"
+        label="Asignar estado"
+        class="mx-16"
+        solo
+        v-model="estado"></v-combobox>
+      <v-btn class="mx-2" depressed color="primary"
+        >Buscar contrato a modificar</v-btn
+      >
+      <v-btn class="mx-2" depressed color="primary" @click="actualizarEstado"
+        >Actualizar Estado</v-btn
+      >
+    </v-container>
   </div>
 </template>
 <script src="./EvaluarContrato.js"></script>
