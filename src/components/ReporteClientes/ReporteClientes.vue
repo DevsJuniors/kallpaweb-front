93f371c9f1a555ef2bb4ae9c6c41c52b725a9531
SC131 <amos27109@gmail.com>
2023-07-01 18:32:13 -0500

    Reporte Contrato, Reporte Cliente

---
+++ src/components/ReporteClientes/ReporteClientes.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <div class="filtros">
+      <v-container class="px-0" fluid>
+        <v-card outlined class="max-width-400">
+          <v-card-title>Filtros</v-card-title>
+          <v-card-text>
+            <p>Selecciona Distrito</p>
+            <v-combobox
+              v-model="distritoSeleccionado"
+              :items="distritos"
+              item-text="Nombre_Di"
+              item-value="IDDistrito"
+              label="Distrito"
+              clearable
+              solo></v-combobox>
+            <p>Fecha</p>
+            <v-row>
+              <v-col cols="12" sm="10">
+                <v-date-picker v-model="start"></v-date-picker>
+              </v-col>
+              <v-col cols="12" sm="10">
+                <v-date-picker v-model="end"></v-date-picker>
+              </v-col>
+            </v-row>
+            <p>Estrato Social</p>
+            <v-combobox
+              v-model="estratoSeleccionado"
+              :items="estratos"
+              item-text="Descripcion_Estrato"
+              item-value="IDEstrato"
+              label="Seleccionar Estrato Social"
+              clearable
+              solo></v-combobox>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" @click="generarReporte"
+              >Generar Reporte</v-btn
+            >
+            <v-btn color="primary" @click="resetFilters">Atras</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </div>
+    <div class="reportec">
+      <v-container>
+        <v-card outlined class="reporte">
+          <v-card-title>Reporte de Clientes</v-card-title>
+          <v-card-text>
+            <textarea rows="20" cols="50" disabled>{{
+              reporteClientes
+            }}</textarea>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </div>
+  </div>
+</template>
+<style>
+.max-width-400 {
+  max-width: 400px;
+}
+.reporte {
+  width: 500px;
+  padding: 20px;
+}
+.filtros {
+  max-width: 400px;
+  margin-right: auto;
+}
+.reportec {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.container {
+  display: flex;
+  justify-content: flex-end;
+}
+textarea {
+  width: 100%;
+  height: 100%;
+  resize: none;
+  border: none;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  color: #333;
+  background-color: #f5f5f5;
+}
+</style>
+<script src="./ReporteClientes.js"></script>
--- src/components/ReporteContrato/ReporteContrato.js
+++ src/components/ReporteContrato/ReporteContrato.js
@@ -3,7 +3,22 @@ export default {
     return {
       estadoSeleccionado: null,
       contratos: [],
+      distritos: [],
+      distritoSeleccionado: null,
+      end: null,
+      start: null,
+      empleadoSeleccionado: null,
+      empleadosCategoria1: [],
+      reporteContratos: "",
     };
+  },
+  watch: {
+    start(newValue) {
+      this.$forceUpdate();
+    },
+    end(newValue) {
+      this.$forceUpdate();
+    },
   },
   computed: {
     estadosContratos() {
@@ -16,6 +31,7 @@ export default {
   },
   mounted() {
     this.obtenerContratos();
+    this.obtenerDistritos();
   },
   methods: {
     obtenerContratos() {
@@ -23,10 +39,80 @@ export default {
         .get("http://localhost:3000/contrato")
         .then((response) => {
           this.contratos = response.data;
+          this.filtrarContratosPorCategoria();
         })
         .catch((error) =>
           console.error("Error al obtener los datos de los contratos : ", error)
         );
+    },
+    obtenerDistritos() {
+      this.axios
+        .get("http://localhost:3000/distrito")
+        .then((response) => {
+          this.distritos = response.data;
+        })
+        .catch((error) =>
+          console.error("Error al obtener los distrito" + error)
+        );
+    },
+    generarReporte() {
+      const contratosFiltrados = this.contratos.filter((contrato) => {
+        const empleado = contrato.empleado;
+        return (
+          empleado &&
+          empleado.IDCategoria === 1 &&
+          (!this.estadoSeleccionado ||
+            contrato.estado === this.estadoSeleccionado) &&
+          (!this.distritoSeleccionado ||
+            contrato.distrito === this.distritoSeleccionado) &&
+          (!this.start || contrato.Fecha_Con >= this.start) &&
+          (!this.end || contrato.Fecha_Con <= this.end)
+        );
+      });
+
+      const nombreAsesor = this.empleadoSeleccionado
+        ? this.empleadoSeleccionado.nombreCompleto
+        : "Todos los Asesores";
+      const estadoContrato = this.estadoSeleccionado || "Todos los Estados";
+      const fechaInicio = this.start || "Sin fecha de inicio";
+      const fechaFin = this.end || "Sin fecha de fin";
+      const distritoSeleccionado =
+        this.distritoSeleccionado || "Todos los Distritos";
+
+      let contenidoReporte = `Nombre del Asesor: ${nombreAsesor}
+      Estado del Contrato: ${estadoContrato}
+      Fecha de Inicio: ${fechaInicio}
+      Fecha de Fin: ${fechaFin}
+      Distrito Seleccionado: ${distritoSeleccionado}
+      
+      Contratos Encontrados: ${contratosFiltrados.length}
+      
+      `;
+      contratosFiltrados.forEach((contrato) => {
+        contenidoReporte += `- ID Contrato: ${contrato.IDContrato}
+      Cliente: ${contrato.DNI_cli}
+      Empleado: ${contrato.DNI_Em}
+      `;
+      });
+
+      this.reporteContratos = contenidoReporte;
+    },
+    filtrarContratosPorCategoria() {
+      this.contratosFiltrados = this.contratos.filter((contrato) => {
+        const empleado = contrato.empleado;
+        return empleado && empleado.IDCategoria === 1;
+      });
+
+      this.empleadosCategoria1 = this.contratosFiltrados.map((contrato) => {
+        const empleado = contrato.empleado;
+        return {
+          DNI_Em: empleado.DNI_Em,
+          nombreCompleto: `${empleado.Nombre_Em} ${empleado.Apellido_Em}`,
+        };
+      });
+    },
+    reporte() {
+      this.$router.push("/reportes");
     },
   },
 };
