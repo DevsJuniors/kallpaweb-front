a70b1157c7d350562822befef94834c4d74e37f9
Yazmin0 <132105807+Yazmin0@users.noreply.github.com>
2023-07-05 21:13:12 -0500

    RegistraEmpleado

--- src/components/RegistrarEmpleado/RegistrarEmpleado.vue
+++ src/components/RegistrarEmpleado/RegistrarEmpleado.vue
@@ -11,86 +11,62 @@
       
         <h2 class = "blue--text">DATOS GENERALES DEL EMPLEADO</h2>
 
-        <v-card class="mx-auto px-6 py-6" max-width="400" variant="outlined">
-
-        
+          <template>
+          <div class="text-field-container">
+            <div class="text-field-wrapper">
         <v-text-field
           label="DNI"
           placeholder="Ingresa el IDGenero"
           filled
           v-model="frmEmpleado.DNI_Em"></v-text-field>
-        
+        </div>
+
+        <div class="text-field-wrapper">
         <v-text-field
           label="Nombre"
           placeholder="Ingresa Nombre"
           filled
           v-model="frmEmpleado.Nombre_Em"></v-text-field>
+        </div>
 
+        <div class="text-field-wrapper">
         <v-text-field
           label="Apellido"
           placeholder="Ingresa Apellido"
           filled
           v-model="frmEmpleado.Apellido_Em"></v-text-field>
-        <v-text-field
+        </div>
+        
+        <div class="text-field-wrapper">
+          <v-text-field
           label="Celular"
           placeholder="Ingresa su numero de celular"
           filled
           v-model="frmEmpleado.Celular_Em"></v-text-field>
+        </div>
         
+        <div class="text-field-wrapper">
           <v-select
           v-model="selectedCategoria"
           :items="categorias.map((categoria) => categoria.Cargo_CE)"
           label="Selecciona la categoria del Empleado"
           outlined
          ></v-select>
-         
-         </v-card>
-         
-        <div>
+        </div>
+        
+          </div>
+        </template>
 
         
-        <v-card-actions class="px-3 pb-3">
-          <v-btn type="submit" class="button-1 mt-2" depressed color="primary">
-            REGISTRAR
-            <v-icon  class="mx-1">mdi-account-badge-outline</v-icon>
-          </v-btn>
-        
-          <v-btn
-              depressed
-              color="primary"
-              class="button-1 mt-2"
-              @click="volverMenu">
-              Atras
-              <v-icon class="mx-1">mdi-keyboard-backspace</v-icon>
-            </v-btn>
-            
-            <v-flex text-ms-right>
-            <v-btn
-              depressed
-              color="primary"
-              class="button-1 mt-2"
-              @click="resetForm">
-              Limpiar
-              <v-icon class="mx-1">mdi-backup-restore</v-icon>
-      </v-btn>
-      </v-flex>
-            <v-btn 
-            type="submit" 
-            class="button-1 mt-2" 
-            depressed color="primary">
-              AGREGAR CREDENCIALES
-            <v-icon class="mx-1">mdi-badge-account-horizontal</v-icon>
-            </v-btn>
-        </v-card-actions> 
-        
-        
-          <form name="credencialesEmpleado" @submit.prevent="createCredenciales">
+        <form name="credencialesEmpleado" @submit.prevent="createCredenciales">
             <div class="credenciales">
+              
+
             <v-card-text>
               <p class="text-xl-left"><h2 class = "blue--text">CREDENCIALES DEL EMPLEADO</h2></p>
             </v-card-text>
-
-            <v-card class="mx-auto px-6 py-8" max-width="400">
+            
+            <div class="text-field-container_2">
               
               <v-text-field
                 label="DNI EMPLEADO"
@@ -108,17 +84,55 @@
                 placeholder="Ingresa una contraseña"
                 filled
                 v-model="frmCredenciales.contraseña"></v-text-field>
-            
-            </v-card>
+      
             </div>
+            </div>
+
+
+          <div class="button-container">
+       
+          <div class="button-row">
+          <v-btn type="submit" class="button-1 mt-2" depressed color="primary">
+            REGISTRAR
+            <v-icon  class="mx-1">mdi-account-badge-outline</v-icon>
+          </v-btn>
+        
+          <v-btn
+              depressed
+              color="primary"
+              class="button-1 mt-2"
+              @click="volverMenu">
+              Atras
+              <v-icon class="mx-1">mdi-keyboard-backspace</v-icon>
+            </v-btn>
+          </div>
+
+          <div class="button-row">
             
+            <v-btn
+              depressed
+              color="primary"
+              class="button-1 mt-2"
+              @click="resetForm">
+              Limpiar
+              <v-icon class="mx-1">mdi-backup-restore</v-icon>
+            </v-btn>
+
+            <v-btn 
+            type="submit" 
+            class="button-1 mt-2" 
+            depressed color="primary">
+              AGREGAR CREDENCIALES
+            <v-icon class="mx-1">mdi-badge-account-horizontal</v-icon>
+            </v-btn>
+          </div>
+        </div>
             
             
             
             
           </form>
         </div>
-      </div>
     </form>
   </div>
 </template>
@@ -145,5 +159,47 @@
   text-align: right;
   margin: 20px;
 }
+
+.text-field-container {
+  display: flex;
+  flex-direction: row;
+  width: 1000px;
+}
+.text-field-container_2{
+  display: flex;
+  flex-direction: right;
+  align-items: center;
+  height: 100vh;
+}
+.spacer {
+  flex-grow: 1;
+}
+.text-field {
+  width: 200px;
+  height: 30px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  padding: 5px;
+}
+.text-field-wrapper {
+  margin-right: 10px;
+}
+.button-container {
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+  top: 50px;
+  right: 10px;
+}
+.button-container_w {
+  
+  
+  justify-content: center;
+}
+.button-row {
+  display: flex;
+  justify-content: center;
+}
 </style>
 <script src="./RegistrarEmpleado.js"></script>
+
