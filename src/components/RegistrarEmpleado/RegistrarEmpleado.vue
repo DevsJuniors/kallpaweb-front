5fe0acfffd78d5e6f40875e6fba125130b64e4ba
Yazmin0 <132105807+Yazmin0@users.noreply.github.com>
2023-07-06 22:47:23 -0500

    Yazmin

--- src/components/RegistrarEmpleado/RegistrarEmpleado.vue
+++ src/components/RegistrarEmpleado/RegistrarEmpleado.vue
@@ -1,7 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <v-div> <!--Cabecera-->
-    <v-app-bar color="white" dense dark height="80">
+  <div>
+    <v-app-bar color="white" dense dark height="100">
       <v-toolbar-title>
         <v-img
           src="../../views/Img/Kallpa.png"
@@ -22,145 +21,12 @@
         </v-col>
       </v-row>
     </v-footer>
-
-    <!--titulo del modulo-->
-    <form name="registrarEmpleado" @submit.prevent="createEmpleado">
-      <v-div class="empleado">
-      
-        <h2 class = "blue--text">DATOS GENERALES DEL EMPLEADO</h2>
-
-        <template><!--caja de texto para dni-->
-          <v-div class="text-field-container">
-            <v-div class="text-field-wrapper">
-        <v-text-field
-          label="DNI"
-          placeholder="Ingresa el IDGenero"
-          filled
-          v-model="frmEmpleado.DNI_Em"></v-text-field>
-        </v-div>
-
-        <!---caja de texto para nombre-->
-        <v-div class="text-field-wrapper">
-        <v-text-field
-          label="Nombre"
-          placeholder="Ingresa Nombre"
-          filled
-          v-model="frmEmpleado.Nombre_Em"></v-text-field>
-        </v-div>
-        
-        <!---caja de texto para apellido-->
-        <v-div class="text-field-wrapper">
-        <v-text-field
-          label="Apellido"
-          placeholder="Ingresa Apellido"
-          filled
-          v-model="frmEmpleado.Apellido_Em"></v-text-field>
-        </v-div>
-      </v-div>
-      <v-div class="text-field-container">
-          <!---caja de texto para celular-->
-          <v-div class="text-field-wrapper">
-          <v-text-field
-          label="Celular"
-          placeholder="Ingresa su numero de celular"
-          filled
-          v-model="frmEmpleado.Celular_Em"></v-text-field>
-        </v-div>
-        
-        <!---combobox para categoria-->
-        <v-div class="text-field-wrapper">
-          <v-select
-          v-model="selectedCategoria"
-          :items="categorias.map((categoria) => categoria.Cargo_CE)"
-          label="Selecciona la categoria del Empleado"
-          outlined
-         ></v-select>
-        </v-div>
-      </v-div>
-        </template>
-
-          <v-div class="button-container flex-column">
-            
-          <v-btn type="submit" class="button-1 mt-2 btn" depressed color="primary">
-            REGISTRAR
-            <v-icon  class="mx-1">mdi-account-badge-outline</v-icon>
-          </v-btn>
-
-          <v-btn
-              depressed
-              color="primary"
-              class="button-1 mt-2 btn"
-              @click="volverMenu">
-              Atras
-              <v-icon class="mx-1">mdi-keyboard-backspace</v-icon>
-            </v-btn>
-
-            <v-btn
-              depressed
-              color="primary"
-              class="button-1 mt-2 btn"
-              @click="resetForm">
-              Limpiar
-              <v-icon class="mx-1">mdi-backup-restore</v-icon>
-            </v-btn>
-
-            <v-btn 
-            type="submit" 
-            class="button-1 mt-2 btn" 
-            depressed color="primary">
-              AGREGAR CREDENCIALES
-            <v-icon class="mx-1">mdi-badge-account-horizontal</v-icon>
-            </v-btn>
-          </v-div>
-          <form name="credencialesEmpleado" @submit.prevent="createCredenciales">
-            <v-div class="credenciales">
-            <h2 class = "blue--text ">CREDENCIALES DEL EMPLEADO</h2>
-
-            <v-div class="text-field-container_2">
-              <v-div class="text-field-wrapper2">
-              <v-text-field
-                label="DNI EMPLEADO"
-                placeholder="Ingresa el DNI del Empleado"
-                filled
-                v-model="frmCredenciales.DNI_Em"></v-text-field>
-              </v-div>
-
-              <v-div class="text-field-wrapper2">
-              <v-text-field
-                label="Nombre de Usuario"
-                placeholder="Ingresa un nombre de usuario"
-                filled
-                v-model="frmCredenciales.nombreusuario"></v-text-field>
-              </v-div>
-
-              <v-div class="text-field-wrapper2">
-              <v-text-field
-                label="Contraseña"
-                placeholder="Ingresa una contraseña"
-                filled
-                v-model="frmCredenciales.contraseña"></v-text-field>
-              </v-div>
-            </v-div>
-            </v-div>
-       
-            
-            
-            
-          </form>
-        </v-div>
-    </form>
-  </v-div>
-</template>
-<style>
-=======
-  <div>
-    <v-app-bar color="green accent-4" dense dark>
-      <v-toolbar-title>KALLPA</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-icon>mdi-account-circle</v-icon>
-    </v-app-bar>
     <div class="spacer"></div>
-    <h2 style="text-align: center">Registro de Empleados</h2>
+    
+    <div style="display: flex; justify-content: center; align-items: center;">
+      <h2 style="color: rgba(0, 0, 129, 0.829); margin-right: 10px;font-size: 30px;font-weight: bold">Registro de</h2>
+      <h2 style="color: rgb(62, 207, 62);font-size: 30px;font-weight: bold">Empleados</h2>
+    </div>
     <v-container fluid>
       <div class="d-flex flex-grow-1">
         <div class="empleado">
@@ -391,19 +257,15 @@
 .custom-text {
   font-size: 12px; /* Modifica el tamaño de letra según tus necesidades */
 }
->>>>>>> c5b04782dac6061a276724237f2f51d2768a9bd8
 .credenciales {
   position: absolute;
-  top: 30px;
+  top: 60px;
   right: 0;
-  margin-right: 320px;
-  margin-top:75px;
-
+  margin-right: 50px;
 }
 .empleado {
   margin: 20px;
   max-width: 30%;
-  margin-left: 300px;
 }
 .button-1 {
   background: #033076;
@@ -417,62 +279,8 @@
   text-align: right;
   margin: 20px;
 }
-
-.text-field-container {
-  display: flex;
-  flex-direction: row;
-  width: 1000px;
-}
-.text-field-container_2{
-  display: flex;
-  flex-direction: row;
-  flex-direction: column;
-  height: 150vh;
-}
-.spacer {
-  flex-grow: 1;
-}
-.text-field {
-  width: 200px;
-  height: 30px;
-  border: 1px solid #000000;
-  border-radius: 4px;
-  padding: 5px;
-}
-.text-field-wrapper {
-  margin-right: 10px;
-}
-.text-field-wrapper2 {
-  margin-right: 100px;
-}
-.button-container {
-  position: absolute;
-  display: flex;
-  flex-wrap: wrap;
-  top: 110px;
-  right: 10px;
-}
-.button-container_w {
-  justify-content: center;
-}
-.button-row {
-  display: flex;
-  justify-content: center;
-}
 .kallpa-image {
   margin-top: 35px;
 }
-.texto-center {
-  text-align: center;
-}
-
-.btn {
-  margin-bottom: 10px;
-  
-}
-
-
 </style>
 <script src="./RegistrarEmpleado.js"></script>
-
-
