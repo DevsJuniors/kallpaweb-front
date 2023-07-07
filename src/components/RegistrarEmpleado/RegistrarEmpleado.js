c5b04782dac6061a276724237f2f51d2768a9bd8
SC131 <amos27109@gmail.com>
2023-07-06 19:12:43 -0500

    Registrar Empleado validaciones

--- src/components/RegistrarEmpleado/RegistrarEmpleado.js
+++ src/components/RegistrarEmpleado/RegistrarEmpleado.js
@@ -14,16 +14,18 @@ export default {
         nombreusuario: "",
         contraseña: "",
       },
-      TextFieldAble:true,
-      search: '',
+      TextFieldAble: true,
+      search: "",
       loading: false,
       search: "",
-      empleados:[],
+      empleados: [],
       categorias: [],
       selectedCategoria: null,
+      mensaje: "",
+      dialogVisible: false,
+      dialogError: false,
     };
   },
-
 
   created() {
     this.getCategoria();
@@ -31,31 +33,32 @@ export default {
 
   // Recuperando ID correspondiente de cada variable
   watch: {
-  selectedCategoria(val){
-          if (val) {
-            const categoria = this.categorias.find((categoria) => categoria.Cargo_CE===val);
-              this.frmEmpleado.IDCategoria = categoria.IDCategoria;
-
-          } else {
-            this.resetForm();
-          }
-   },
-},
-computed: {
-  headers() {
-    return [
-      { text: "DNI ", value: "DNI_Em" },
-      { text: "Categoria", value: "IDCategoria" },
-      { text: "Nombres", value: "Nombre_Em" },
-      { text: "Apellidos", value: "Apellido_Em" },
-      { text: "Celular", value: "Celular_Em" },
-      { text: "Seleccionar", value: "" },
-    ];
+    selectedCategoria(val) {
+      if (val) {
+        const categoria = this.categorias.find(
+          (categoria) => categoria.Cargo_CE === val
+        );
+        this.frmEmpleado.IDCategoria = categoria.IDCategoria;
+      } else {
+        this.resetForm();
+      }
+    },
   },
-},
-mounted() {
-  this.obtenerEmpleados();
-},
+  computed: {
+    headers() {
+      return [
+        { text: "DNI ", value: "DNI_Em" },
+        { text: "Categoria", value: "IDCategoria" },
+        { text: "Nombres", value: "Nombre_Em" },
+        { text: "Apellidos", value: "Apellido_Em" },
+        { text: "Celular", value: "Celular_Em" },
+        { text: "Seleccionar", value: "" },
+      ];
+    },
+  },
+  mounted() {
+    this.obtenerEmpleados();
+  },
   methods: {
     obtenerEmpleados() {
       this.axios
@@ -66,84 +69,200 @@ mounted() {
         .catch((error) =>
           console.error("Error al obtener los datos de los empleados: ", error)
         );
-  },   
+    },
     createEmpleado() {
-      this.axios
-        .post("http://localhost:3000/empleado", this.frmEmpleado)
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((e) => e);
-        this.resetForm()
+      if (
+        !this.frmEmpleado.DNI_Em ||
+        !this.frmEmpleado.Nombre_Em ||
+        !this.frmEmpleado.Apellido_Em ||
+        !this.frmEmpleado.Celular_Em ||
+        !this.frmEmpleado.IDCategoria
+      ) {
+        let camposFaltantes = "";
+        if (!this.frmEmpleado.DNI_Em) {
+          camposFaltantes += "DNI Empleado, ";
+        }
+        if (!this.frmEmpleado.Nombre_Em) {
+          camposFaltantes += "Nombre, ";
+        }
+        if (!this.frmEmpleado.Celular_Em) {
+          camposFaltantes += "Celular, ";
+        }
+        if (!this.frmEmpleado.IDCategoria) {
+          camposFaltantes += "Categoria. ";
+        }
+
+        camposFaltantes = camposFaltantes.slice(0, -2);
+        this.mensaje = `Faltan campos por rellenar, por favor verificar que sus datos esten completos :${camposFaltantes}`;
+        this.typemsg = "error";
+        this.dialogError = true;
+      } else {
+        this.axios
+          .post("http://localhost:3000/empleado", this.frmEmpleado)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => e);
+        this.resetFormE();
+        this.mensaje =
+          "Se registro al empleado correctamente (PORFAVOR REGISTRAR UNA CREDENCIAL PARA EL EMPLEADO)" +
+          this.frmEmpleado.Nombre_Em;
+        this.typemsg = "success";
+        this.dialogVisible = true;
+      }
+    },
+    aceptar() {
+      // Lógica para manejar la acción de aceptar en el diálogo
+      this.dialogVisible = false;
+      this.resetForm();
     },
     updateCliente() {
-      const dniempleado = this.frmEmpleado.DNI_Em;
-      const dataUpdate = {
-        DNI_Em: this.frmEmpleado.DNI_Em,
-        Nombre_Em: this.frmEmpleado.Nombre_Em,
-        Apellido_Em: this.frmEmpleado.Apellido_Em,
-        Celular_Em: this.frmEmpleado.Celular_Em,
-        IDCategoria: this.frmEmpleado.IDCategoria,
-      };
-      this.axios
-        .patch(`http://localhost:3000/empleado/${dniempleado}`, dataUpdate)
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((e) => e);
-      this.obtenerEmpleados();
-      this.resetForm()
-      this.TextFieldAble=true;
-      return window.location.reload();
+      if (
+        !this.frmEmpleado.DNI_Em ||
+        !this.frmEmpleado.Nombre_Em ||
+        !this.frmEmpleado.Apellido_Em ||
+        !this.frmEmpleado.Celular_Em ||
+        !this.frmEmpleado.IDCategoria
+      ) {
+        let camposFaltantes = "";
+        if (!this.frmEmpleado.DNI_Em) {
+          camposFaltantes += "DNI Empleado, ";
+        }
+        if (!this.frmEmpleado.Nombre_Em) {
+          camposFaltantes += "Nombre, ";
+        }
+        if (!this.frmEmpleado.Apellido_Em) {
+          camposFaltantes += "Apellido, ";
+        }
+        if (!this.frmEmpleado.Celular_Em) {
+          camposFaltantes += "Celular, ";
+        }
+        if (!this.frmEmpleado.IDCategoria) {
+          camposFaltantes += "Categoria. ";
+        }
+
+        camposFaltantes = camposFaltantes.slice(0, -2);
+        this.mensaje = `Faltan campos por rellenar, por favor verificar que sus datos esten completos :${camposFaltantes}`;
+        this.typemsg = "error";
+        this.dialogError = true;
+      } else {
+        const dniempleado = this.frmEmpleado.DNI_Em;
+        const dataUpdate = {
+          DNI_Em: this.frmEmpleado.DNI_Em,
+          Nombre_Em: this.frmEmpleado.Nombre_Em,
+          Apellido_Em: this.frmEmpleado.Apellido_Em,
+          Celular_Em: this.frmEmpleado.Celular_Em,
+          IDCategoria: this.frmEmpleado.IDCategoria,
+        };
+        this.axios
+          .patch(`http://localhost:3000/empleado/${dniempleado}`, dataUpdate)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => e);
+        this.obtenerEmpleados();
+        this.resetForm();
+        this.TextFieldAble = true;
+        this.dialogVisible = true;
+        return window.location.reload();
+      }
     },
     createCredenciales() {
-      this.axios
-        .post(
-          "http://localhost:3000/credenciales-empleado",
-          this.frmCredenciales
-        )
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((e) => e);
+      if (
+        !this.frmCredenciales.DNI_Em ||
+        !this.frmCredenciales.nombreusuario ||
+        !this.frmCredenciales.contraseña
+      ) {
+        let camposFaltantes = "";
+
+        if (!this.frmCredenciales.DNI_Em) {
+          camposFaltantes += "DNI Empleado, ";
+        }
+        if (!this.frmCredenciales.nombreusuario) {
+          camposFaltantes += "Nombre de usuario, ";
+        }
+        if (!this.frmCredenciales.contraseña) {
+          camposFaltantes += "Contraseña, ";
+        }
+
+        camposFaltantes = camposFaltantes.slice(0, -2); // Eliminar la coma y el espacio al final
+
+        this.mensaje = `Faltan datos por completar en las credenciales: ${camposFaltantes}`;
+        this.typemsg = "error";
+        this.dialogError = true;
+      } else {
+        this.axios
+          .post(
+            "http://localhost:3000/credenciales-empleado",
+            this.frmCredenciales
+          )
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => e);
+        this.mensaje =
+          "Se registraron las credenciales del empleado " +
+          this.frmCredenciales.nombreusuario;
+        this.typemsg = "success";
+        this.dialogVisible = true;
+      }
     },
-    seleccionarEmpleado(empleado){
+    seleccionarEmpleado(empleado) {
       this.frmEmpleado.DNI_Em = empleado.DNI_Em;
-      this.frmEmpleado.Nombre_Em=empleado.Nombre_Em;
-      this.frmEmpleado.Apellido_Em=empleado.Apellido_Em;
-      this.frmEmpleado.Celular_Em=empleado.Celular_Em;
-      this.selectedCategoria= this.encontrarCategoria(empleado.IDCategoria);
-      this.TextFieldAble=false
-  },
+      this.frmEmpleado.Nombre_Em = empleado.Nombre_Em;
+      this.frmEmpleado.Apellido_Em = empleado.Apellido_Em;
+      this.frmEmpleado.Celular_Em = empleado.Celular_Em;
+      this.selectedCategoria = this.encontrarCategoria(empleado.IDCategoria);
+      this.TextFieldAble = false;
+    },
+    cerrar() {
+      this.dialogVisible = false;
+      this.dialogError = false;
+      this.resetForm();
+    },
     getCategoria() {
       this.axios
         .get("http://localhost:3000/categoria-empleado")
         .then((res) => {
-          this.categorias = res.data.filter((categoria) => categoria.IDCategoria !== 7);
+          this.categorias = res.data.filter(
+            (categoria) => categoria.IDCategoria !== 7
+          );
         })
         .catch((error) => e);
     },
-    encontrarCategoria(number){
-      var categoria = this.categorias.find((categoria) => categoria.IDCategoria === number);
+    encontrarCategoria(number) {
+      var categoria = this.categorias.find(
+        (categoria) => categoria.IDCategoria === number
+      );
       if (categoria) {
         var cargo = categoria.Cargo_CE;
         return cargo;
-    } else {
+      } else {
         return null; // o algún valor predeterminado si la categoría no se encuentra
-    }
+      }
     },
     volverMenu() {
       this.$router.push("/menu");
     },
-    resetForm() {
+    resetFormE() {
       this.frmEmpleado.DNI_Em = "";
       this.frmEmpleado.Nombre_Em = "";
       this.frmEmpleado.Apellido_Em = "";
-      this.frmEmpleado.Celular_Em= "";
-      this.frmCredenciales.DNI_Em= "";
-      this.frmCredenciales.nombreusuario= "";
-      this.frmCredenciales.contraseña= "";
-      this.selectedCategoria= null;
+      this.frmEmpleado.Celular_Em = "";
+      this.selectedCategoria = null;
+    },
+    resetFormC() {
+      this.frmCredenciales.DNI_Em = "";
+      this.frmCredenciales.nombreusuario = "";
+      this.frmCredenciales.contraseña = "";
+    },
+    resetForm() {
+      this.resetFormE();
+      this.resetFormC();
+    },
+    aceptar() {
+      this.dialogVisible = false;
+      this.resetForm();
     },
   },
 };
--- src/components/RegistrarEmpleado/RegistrarEmpleado.vue
+++ src/components/RegistrarEmpleado/RegistrarEmpleado.vue
@@ -1,153 +1,210 @@
 <template>
   <div>
     <v-app-bar color="green accent-4" dense dark>
-            <v-toolbar-title>KALLPA</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-icon>mdi-account-circle</v-icon>
+      <v-toolbar-title>KALLPA</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-account-circle</v-icon>
     </v-app-bar>
     <div class="spacer"></div>
-      <h2 style=" text-align: center;">Registro de Empleados </h2> 
+    <h2 style="text-align: center">Registro de Empleados</h2>
     <v-container fluid>
       <div class="d-flex flex-grow-1">
         <div class="empleado">
           <h3>Datos Generales</h3>
           <div class="contenedor-cajas">
-              <v-text-field
-                label="DNI"
-                placeholder="Ingresa el IDGenero"
-                filled
-                :disabled="!TextFieldAble"
-                v-model="frmEmpleado.DNI_Em"></v-text-field>
-              <v-text-field
-                label="Nombre"
-                placeholder="Ingresa Nombre"
-                filled
-                v-model="frmEmpleado.Nombre_Em"></v-text-field>
-              <v-text-field
-                label="Apellido"
-                placeholder="Ingresa Apellido"
-                filled
-                v-model="frmEmpleado.Apellido_Em"></v-text-field>
-              <v-text-field
-                label="Celular"
-                placeholder="Ingresa su numero de celular"
-                filled
-                v-model="frmEmpleado.Celular_Em"></v-text-field>
-                <v-select
-                v-model="selectedCategoria"
-                :items="categorias.map((categoria) => categoria.Cargo_CE)"
-                label="Selecciona la categoria del Empleado"
-                outlined
-              ></v-select>
-              <v-btn  
-                depressed
-                style="background-color: #47d847; color: #ffffff;"
-                class="mt-2 mb-4 x-large"
-                @click="createEmpleado">
-                REGISTRAR
-                <v-icon  class="mx-1">mdi-account-badge-outline</v-icon>
-              </v-btn>
+            <v-text-field
+              label="DNI"
+              placeholder="Ingresa el IDGenero"
+              filled
+              :disabled="!TextFieldAble"
+              v-model="frmEmpleado.DNI_Em"></v-text-field>
+            <v-text-field
+              label="Nombre"
+              placeholder="Ingresa Nombre"
+              filled
+              v-model="frmEmpleado.Nombre_Em"></v-text-field>
+            <v-text-field
+              label="Apellido"
+              placeholder="Ingresa Apellido"
+              filled
+              v-model="frmEmpleado.Apellido_Em"></v-text-field>
+            <v-text-field
+              label="Celular"
+              placeholder="Ingresa su numero de celular"
+              filled
+              v-model="frmEmpleado.Celular_Em"></v-text-field>
+            <v-select
+              v-model="selectedCategoria"
+              :items="categorias.map((categoria) => categoria.Cargo_CE)"
+              label="Selecciona la categoria del Empleado"
+              outlined></v-select>
+            <v-btn
+              depressed
+              style="background-color: #47d847; color: #ffffff"
+              class="mt-2 mb-4 x-large"
+              @click="createEmpleado">
+              REGISTRAR
+              <v-icon class="mx-1">mdi-account-badge-outline</v-icon>
+            </v-btn>
           </div>
-    </div>
-      <div class="tabla fill-height">
-          <v-data-table 
-            :headers="headers" 
+          <v-dialog v-model="dialogVisible" :width="500">
+            <v-card color="#47d847">
+              <v-card-title>
+                <span class="mx-auto" style="color: white"
+                  >¡Registrado con Exito!</span
+                >
+              </v-card-title>
+              <v-card-text>
+                <v-alert
+                  v-if="mensaje !== ''"
+                  color="white"
+                  :type="typemsg"
+                  outlined
+                  >{{ mensaje }}</v-alert
+                >
+              </v-card-text>
+              <v-card-actions style="display: flex; justify-content: center">
+                <v-btn
+                  style="background-color: #033076; color: #ffffff"
+                  @click="aceptar">
+                  Aceptar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+        <div class="tabla fill-height">
+          <v-data-table
+            :headers="headers"
             :items="empleados"
             :search="search"
             :loading="loading"
-            :items-per-page="5" 
+            :items-per-page="5"
             class="custom-table">
             <template v-slot:top>
-                     <v-toolbar flat>
-                     <v-toolbar-title> Lista de Empleados</v-toolbar-title>
-                     <v-spacer></v-spacer>
-                    <v-text-field
-                    v-model="search"
-                    append-icon="mdi-magnify"
-                    label="Buscar"
-                    single-line
-                    hide-details
-                    ></v-text-field>
-                    </v-toolbar>
+              <v-toolbar flat>
+                <v-toolbar-title> Lista de Empleados</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Buscar"
+                  single-line
+                  hide-details></v-text-field>
+              </v-toolbar>
             </template>
-            <template v-slot:item="{item}">
+            <template v-slot:item="{ item }">
               <tr>
                 <td>{{ item.DNI_Em }}</td>
                 <td>{{ item.IDCategoria }}</td>
                 <td>{{ item.Nombre_Em }}</td>
                 <td>{{ item.Apellido_Em }}</td>
                 <td>{{ item.Celular_Em }}</td>
-                <td>{{ item.Seleccionar}}
-                        <v-btn small color="primary" @click="seleccionarEmpleado(item)">
-                        <v-icon class="mx-1">mdi-arrange-bring-forward</v-icon>
-                        </v-btn>
+                <td>
+                  {{ item.Seleccionar }}
+                  <v-btn
+                    small
+                    color="primary"
+                    @click="seleccionarEmpleado(item)">
+                    <v-icon class="mx-1">mdi-arrange-bring-forward</v-icon>
+                  </v-btn>
                 </td>
-                </tr>
+              </tr>
             </template>
           </v-data-table>
           <div class="contenedor-cajas-E my-4">
-            <h3 style="color: #47d847; text-align: center;">Credenciales </h3> 
+            <h3 style="color: #47d847; text-align: center">Credenciales</h3>
             <v-text-field
-                label="DNI EMPLEADO"
-                placeholder="Ingresa el DNI del Empleado"
-                filled
-                :disabled="!TextFieldAble"
-                style="display: inline-block; width: 18%;margin-right: 8px;"
-                v-model="frmCredenciales.DNI_Em"></v-text-field>
+              label="DNI EMPLEADO"
+              placeholder="Ingresa el DNI del Empleado"
+              filled
+              :disabled="!TextFieldAble"
+              style="display: inline-block; width: 18%; margin-right: 8px"
+              v-model="frmCredenciales.DNI_Em"></v-text-field>
             <v-text-field
-                label="Nombre de Usuario"
-                placeholder="Ingresa un nombre de usuario"
-                filled
-                :disabled="!TextFieldAble"
-                style="display: inline-block; width: 25%;margin-right: 8px;"
-                v-model="frmCredenciales.nombreusuario"></v-text-field>
-                <v-text-field
-                label="Contraseña"
-                placeholder="Ingresa una contraseña"
-                filled
-                :disabled="!TextFieldAble"
-                style="display: inline-block; width: 25%;margin-right: 8px;"
-                v-model="frmCredenciales.contraseña"></v-text-field>
-                <v-btn  depressed
-                    style="background-color: #47d847; color: #ffffff;display: inline-block; width: 25%;"
-                    class="mt-2 mb-4 x-large"
-                    @click="createCredenciales">
-                    AGREGAR CREDENCIALES
-                    <v-icon class="mx-1">mdi-badge-account-horizontal</v-icon>
-               </v-btn>
-        </div>
-              <v-btn
-                  depressed
-                  color="primary"
-                  class="button-1 mt-2"
-                  @click="updateCliente">
-                  Actualizar
-                  <v-icon class="mx-1">mdi-account-edit</v-icon>
-              </v-btn>
-              <v-btn
-                  depressed
-                  color="primary"
-                  class="button-1 mt-2"
-                  @click="volverMenu">
-                  Eliminar
-                  <v-icon class="mx-1">mdi-delete-empty</v-icon>
-              </v-btn>
-              <v-btn
-                  depressed
-                  color="primary"
-                  class="button-1 mt-2"
-                  @click="volverMenu">
-                  Atras
-                  <v-icon class="mx-1">mdi-keyboard-backspace</v-icon>
-              </v-btn>
-              <v-btn
-                  depressed
-                  color="primary"
-                  class="button-1 mt-2"
-                  @click="resetForm">
-                  Limpiar
-                  <v-icon class="mx-1">mdi-backup-restore</v-icon>
-              </v-btn>
+              label="Nombre de Usuario"
+              placeholder="Ingresa un nombre de usuario"
+              filled
+              :disabled="!TextFieldAble"
+              style="display: inline-block; width: 25%; margin-right: 8px"
+              v-model="frmCredenciales.nombreusuario"></v-text-field>
+            <v-text-field
+              label="Contraseña"
+              placeholder="Ingresa una contraseña"
+              filled
+              :disabled="!TextFieldAble"
+              style="display: inline-block; width: 25%; margin-right: 8px"
+              v-model="frmCredenciales.contraseña"></v-text-field>
+            <v-btn
+              depressed
+              style="
+                background-color: #47d847;
+                color: #ffffff;
+                display: inline-block;
+                width: 25%;
+              "
+              class="mt-2 mb-4 x-large"
+              @click="createCredenciales">
+              AGREGAR CREDENCIALES
+              <v-icon class="mx-1">mdi-badge-account-horizontal</v-icon>
+            </v-btn>
+          </div>
+          <v-dialog v-model="dialogError" :width="500">
+            <v-card color="#ec4a4a">
+              <v-card-title>
+                <span class="mx-auto" style="color: white"> ¡Verifique! </span>
+              </v-card-title>
+              <v-card-text>
+                <v-alert
+                  v-if="mensaje !== ''"
+                  color="white"
+                  :type="typemsg"
+                  outlined
+                  >{{ mensaje }}</v-alert
+                >
+              </v-card-text>
+              <v-card-actions style="display: flex; justify-content: center">
+                <v-btn
+                  style="background-color: #033076; color: #ffffff"
+                  @click="cerrar">
+                  cerrar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <v-btn
+            depressed
+            color="primary"
+            class="button-1 mt-2"
+            @click="updateCliente">
+            Actualizar
+            <v-icon class="mx-1">mdi-account-edit</v-icon>
+          </v-btn>
+          <v-btn
+            depressed
+            color="primary"
+            class="button-1 mt-2"
+            @click="volverMenu">
+            Eliminar
+            <v-icon class="mx-1">mdi-delete-empty</v-icon>
+          </v-btn>
+          <v-btn
+            depressed
+            color="primary"
+            class="button-1 mt-2"
+            @click="volverMenu">
+            Atras
+            <v-icon class="mx-1">mdi-keyboard-backspace</v-icon>
+          </v-btn>
+          <v-btn
+            depressed
+            color="primary"
+            class="button-1 mt-2"
+            @click="resetForm">
+            Limpiar
+            <v-icon class="mx-1">mdi-backup-restore</v-icon>
+          </v-btn>
         </div>
       </div>
     </v-container>
@@ -155,15 +212,15 @@
 </template>
 <style>
 .custom-table thead th {
-    background-color: #095ba8;
-    color: #ffffff !important; 
+  background-color: #095ba8;
+  color: #ffffff !important;
 }
 .contenedor-cajas {
   border: 2px solid #47d847;
   padding: 10px;
 }
 .contenedor-cajas-E {
-  border: 2px solid #47d847 ;
+  border: 2px solid #47d847;
   padding: 10px;
 }
 .contenedor-cajas input {
